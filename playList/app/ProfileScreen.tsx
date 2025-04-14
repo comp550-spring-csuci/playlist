e563ce8355f3eeeab795911d2f19e4f850c2f059
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-14 00:26:33 -0700

    Files for bottom tab navigation

---
+++ playList/app/ProfileScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
