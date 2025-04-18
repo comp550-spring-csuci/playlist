0801e4a821d8068af66192a1e3b1fe56ab73641e
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-17 21:37:20 -0700

    Modified files.

--- playList/app/dashboard.tsx
+++ playList/app/dashboard.tsx
@@ -1,35 +1,46 @@
-import React from "react";
-import { View, Text, TouchableOpacity, ImageBackground } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ImageBackground } from "react-native";
 import { Link } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "@/styles/style";
 
-const Dashboard = () => {
-  const tiles = [
-    { title: "Songs", icon: "musical-notes", href: "/songs" },
-    { title: "Podcasts", icon: "mic", href: "/podcasts" },
-    { title: "Audiobooks", icon: "book", href: "/audiobooks" },
-    { title: "Videos", icon: "videocam", href: "/videos" },
-  ];
+import * as React from 'react';
+import TabNavigator from './TabNavigator';
+import { NavigationContainer } from "@react-navigation/native";
 
+
+const tiles = [
+  { title: "Songs", icon: "musical-notes", href: "/songs" },
+  { title: "Podcasts", icon: "mic", href: "/podcasts" },
+  { title: "Audiobooks", icon: "book", href: "/audiobooks" },
+  { title: "Videos", icon: "videocam", href: "/videos" },
+];
+
+const Dashboard:React.FC = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.overlay}>
-        <Text style={styles.dashboardHeader}>Dashboard</Text>
-        <View style={styles.tilesContainer}>
-          {tiles.map((tile, index) => (
-            <Link key={index} href={tile.href} asChild>
-              <TouchableOpacity style={styles.tile} activeOpacity={0.8}>
-                <Ionicons name={tile.icon} size={50} color="#fff" />
-                <Text style={styles.tileText}>{tile.title}</Text>
-              </TouchableOpacity>
-            </Link>
-          ))}
-        </View>
+      {/*<NavigationContainer>
+        <TabNavigator/>
+      </NavigationContainer>*/}
+    <View style={styles.overlay}>
+      <Text style={styles.dashboardHeader}>Dashboard</Text>
+      <View style={styles.tilesContainer}>
+        {tiles.map((tile, index) => (
+          <Link key={index} href={tile.href} asChild>
+            <TouchableOpacity style={styles.tile} activeOpacity={0.8}>
+              <Ionicons name={tile.icon} size={50} color="#fff" />
+              <Text style={styles.tileText}>{tile.title}</Text>
+            </TouchableOpacity>
+          </Link>
+        ))}
       </View>
-    </SafeAreaView>
+     
+    </View>
+  </SafeAreaView>
+    
   );
-};
+
+}
+
 
 export default Dashboard;
--- playList/app/index.tsx
+++ playList/app/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View, Image } from "react-native";
+import * as React from 'react';
+import { Text, View, Image, StyleSheet } from "react-native";
 import { useRouter } from "expo-router";
 import MyButton from "@/components/MyButton"
-import { styles } from "@/styles/style";
+//import { styles } from "@/styles/style";
+
+
+
 
 
 export default function Index() {
@@ -14,17 +18,56 @@ export default function Index() {
 
   return (
     <View style={styles.indexContainer}>
-      <View>
-        <Image source={require("@/assets/images/myicon.png")} />
-        <Text style={styles.title}>PlayList</Text>
-      </View>
+      
+        {/*<Text style={styles.title}>PlayList</Text>*/}
+        <View style={styles.imageContainer}>
+          <Image source={require("@/assets/images/myicon.png")} />
+          <Image source={require("@/assets/images/playlist-logo.png")} style={styles.image} />
+        </View>
+      
       <View style={styles.formContainer}>
-        <Text style={styles.welcomeText}>Welcome to PlayList</Text>
+       {/*} <Text style={styles.welcomeText}>Welcome to PlayList</Text>
         <Text style={styles.description}>Lorem ipsum is a dummy or placeholder text commonly used
         in graphic design, publishing, and web development to fill empty spaces in a layout that
-        does not yet have content.</Text>
+        does not yet have content.</Text> */}
         <MyButton title="GET STARTED" onPress={getStarted} />
-      </View>
+      </View> 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  indexContainer: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image: {
+    width: 350,
+    height: 350,
+    resizeMode: 'contain'
+  },
+
+  title: {
+    textAlign: 'center'
+  },
+
+    formContainer: {
+      alignItems: 'center'
+    },
+  
+    welcomeText: {
+      textAlign: 'center'
+    },
+
+    description: {
+      textAlign: 'center'
+    }
+  }
+)
