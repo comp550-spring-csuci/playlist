0afbc0d160649f94d12d991241918ad686d89d5e
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-24 13:03:23 -0700

    New tab bar layout and corresponding screens.

--- playList/app/TabNavigator.tsx
+++ playList/app/TabNavigator.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native'; 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
-import HomeScreen from './screens/HomeScreen';
-import SettingsScreen from './screens/SettingsScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './dashboard'; 
+import PlaylistScreen from './screens/PlaylistScreen';
+import DownloadsScreen from './screens/DownloadsScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import Dashboard from './dashboard';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator:React.FC = () => {
   return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen}/>
-        <Tab.Screen name="Dashboard" component={Dashboard}/>
+        <Tab.Screen name="Home" component={Dashboard}/>
+        <Tab.Screen name="Playlists" component={PlaylistScreen}/>
+        <Tab.Screen name="Downloads" component={DownloadsScreen}/>
         <Tab.Screen name="Profile" component={ProfileScreen}/>
-        <Tab.Screen name="Settings" component={SettingsScreen}/>
       </Tab.Navigator>
 );
 }
---
+++ playList/app/screens/DownloadsScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function DownloadsScreen() {
+  return (
+    <View>
+      <Text>Downloads Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
