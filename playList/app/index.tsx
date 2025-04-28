3d959bb5c717882309f1991d5768c86010b2b2e1
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-27 19:04:15 -0700

    Added space between text and Get Started button.

--- playList/app/index.tsx
+++ playList/app/index.tsx
@@ -20,17 +20,29 @@ export default function Index() {
     <View style={styles.indexContainer}>
       
         {/*<Text style={styles.title}>PlayList</Text>*/}
-        <View style={styles.imageContainer}>
-          <Image source={require("@/assets/images/myicon.png")} />
-          <Image source={require("@/assets/images/playlist-logo.png")} style={styles.image} />
+        <View style={styles.container}>
+
+          <Image 
+            source={require("@/assets/images/myicon.png")}
+            style= {styles.firstImage} 
+          />
+
+          <Text style={styles.welcomeText}>Welcome to</Text>
+
+
+          <Image 
+            source={require("@/assets/images/playlist-logo.png")} 
+            style={styles.secondImage} />
         </View>
       
       <View style={styles.formContainer}>
-        <Text style={styles.welcomeText}>Welcome to PlayList</Text>
+        {/*<Text style={styles.welcomeText}>Welcome to PlayList</Text>*/}
         <Text style={styles.description}>Your music. Your mood. Your PlayList.{"\n"}
         Explore songs, podcasts, audiobooks, and videos come together seamlessly —
         all in one beautifully personalized experience.{"\n"}PlayList is your all-in-one media companion.</Text>
-        <MyButton title="GET STARTED" onPress={getStarted} />
+        <View style = {styles.buttonContainer}>
+          <MyButton title="GET STARTED" onPress={getStarted} />
+        </View>
       </View> 
     </View>
   );
@@ -43,15 +55,20 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
 
-  imageContainer: {
+  container: {
     justifyContent: 'center',
     alignItems: 'center',
   },
 
-  image: {
+  firstImage: {
+    marginBottom: 20
+  },
+
+  secondImage: {
     width: 350,
     height: 350,
-    resizeMode: 'contain'
+    resizeMode: 'contain',
+    marginTop: 10
   },
 
   title: {
@@ -62,8 +79,13 @@ const styles = StyleSheet.create({
       alignItems: 'center'
     },
   
+    buttonContainer: {
+      marginTop: 30
+    },
+
     welcomeText: {
-      textAlign: 'center'
+      textAlign: 'center',
+      marginBottom: 5
     },
 
     description: {
