8fa3cc8a8396f214930635fcf0606fe2b4a56a26
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-27 18:09:34 -0700

    Tab Navigator now has home icon.

--- playList/app/TabNavigator.tsx
+++ playList/app/TabNavigator.tsx
@@ -5,13 +5,34 @@ import Dashboard from './dashboard';
 import Playlists from './screens/Playlists';
 import Downloads from './screens/Downloads';
 import UserProfile from './screens/UserProfile';
+import Entypo from '@expo/vector-icons/Entypo';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigator:React.FC = () => {
   return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Dashboard}/>
+      <Tab.Navigator screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#f5f5f5',
+          borderTopWidth: 0,
+          elevation: 4
+        }
+      }}
+      >
+        <Tab.Screen 
+          name="Home"
+          component={Dashboard}
+          options = {{
+              tabBarLabel: "Home",
+              headerShown: false,
+              tabBarLabelStyle: {color: "white"},
+              tabBarIcon: ({focused}) =>
+                focused? (
+                  <Entypo name="home" size={24} color="black" />
+                ): (<AntDesign name="home" size={24} color="black" />) }} 
+        />
+
         <Tab.Screen name="Playlists" component={Playlists}/>
         <Tab.Screen name="Downloads" component={Downloads}/>
         <Tab.Screen name="Profile" component={UserProfile}/>
