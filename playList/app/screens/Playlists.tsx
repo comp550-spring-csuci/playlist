66c9c36ac2f7af5180b6538299099a84bcc5695f
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-30 11:09:14 -0700

    Playlists screen now has a white background and no longer shows text header.

--- playList/app/screens/Playlists.tsx
+++ playList/app/screens/Playlists.tsx
@@ -1,15 +1,27 @@
 
 import { Text, View, StyleSheet } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Playlists() {
   return (
-    <View>
-      <Text>Playlist Screen</Text>
-    </View>
+
+  
+      <View style={styles.indexContainer}></View>
+
   );
 }
 
 const styles = StyleSheet.create({
+
+  indexContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 40,
+    gap: 140,
+    backgroundColor: "white",
+  }
+
 
 });
