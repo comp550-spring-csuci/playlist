ad0b76bc6be424ca02000371aab997a6a771cc57
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-04-30 17:57:08 -0700

    Login screen with buttons, still a work in progress.

--- playList/app/login.tsx
+++ playList/app/login.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, Image, TextInput, TouchableOpacity, Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import MyButton from "@/components/MyButton";
 import { styles } from "@/styles/style";
+import Entypo from '@expo/vector-icons/Entypo';
 
 const Login = () => {
   const router = useRouter();
@@ -23,17 +24,31 @@ const Login = () => {
         <Text style={styles.title}>PlayList</Text>
       </View>
       <View style={styles.formContainer}>
-        <TextInput
-          placeholder="Email Address"
-          style={styles.input}
-          onChangeText={(e) => console.log(e)}
-        />
-        <TextInput
-          placeholder="Password"
-          style={styles.input}
-          onChangeText={(e) => console.log(e)}
-        />
-        <MyButton title="LOGIN" onPress={onLogin} />
+        
+        <MyButton title="Login with Spotify" onPress={onLogin} />
+
+       
+        <Pressable
+          onPress={onLogin}
+          style={{
+            backgroundColor: "#1DB954",
+            paddingHorizontal: 60,
+            paddingVertical: 15,
+            borderRadius: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            marginLeft: "auto",
+            marginRight: "auto",
+            flexDirection:"row"
+          }}
+        >
+        <Text style={{ fontSize: 16, color: "white", marginRight: 8}}>Login with Spotify</Text>
+        <Entypo name="spotify" size={24} color="black" />
+        </Pressable>
+
+
+
+        
       </View>
       <TouchableOpacity onPress={onSignUp}>
         <Text style={styles.signUpText}>Don't Have an Account? Sign Up</Text>
