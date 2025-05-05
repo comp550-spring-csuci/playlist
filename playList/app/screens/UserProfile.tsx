0feb556378e504a51482697b8ba47ccb0aaf42ec
erikamanley451 <erika.manley451@myci.csuci.edu>
2025-05-04 17:51:18 -0700

    User Profile screen now has a white background and no header text.

--- playList/app/screens/UserProfile.tsx
+++ playList/app/screens/UserProfile.tsx
@@ -1,13 +1,25 @@
 import { Text, View, StyleSheet } from 'react-native';
 
 export default function UserProfile() {
+  
   return (
-    <View>
-      <Text>Profile Screen</Text>
-    </View>
-  );
+
+  
+    <View style={styles.indexContainer}></View>
+
+);
 }
 
 const styles = StyleSheet.create({
+
+indexContainer: {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  padding: 40,
+  gap: 140,
+  backgroundColor: "white",
+}
+
 
 });
