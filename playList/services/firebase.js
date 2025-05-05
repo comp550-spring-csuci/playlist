af092117c77df6bb65a13a260a6629758cf11bcd
harpreetkaur-hk <harpreetkaur180027@gmail.com>
2025-05-04 20:08:37 -0700

    Removed FireBase Credentials for Security

--- playList/services/firebase.js
+++ playList/services/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLEt4L86XH_1OiZ4Xo9xXQ-zvrw-GeHbc",
-  authDomain: "playlist-bb15e.firebaseapp.com",
-  projectId: "playlist-bb15e",
-  storageBucket: "playlist-bb15e.firebasestorage.app",
-  messagingSenderId: "262236138318",
-  appId: "1:262236138318:web:820431dc15a35fd7730c1f",
-  measurementId: "G-YSCLZPQ50W"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
