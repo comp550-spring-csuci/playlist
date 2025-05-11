8641ebfc4c31a0faf15dba658cc56cca7e0c08db
JasonChabot-rar <jrchabot@uci.edu>
2025-05-11 12:54:18 -0700

    comments explaining apple api functions

--- playList/services/appleService.ts
+++ playList/services/appleService.ts
@@ -1,11 +1,19 @@
 const APPLE_MUSIC_DEVELOPER_TOKEN = 'eyJhbGciOiJFUzI1NiIsImtpZCI6Ikg2UDVLN1RGNFAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJCREhVQkpOVTlDIiwiaWF0IjoxNzQ1MjgzNDc0LCJleHAiOjE3NTA1Mzk0NzR9.4vx5venrU2HIsxTS5GoMksfVckabBQVnSgyXVLy49QgwWnNx-3ezc3oUxHsjkX2BWnh-iN1KuFGFXyqcs0hcQg';
 const APPLE_MUSIC_API_URL = 'https://api.music.apple.com/v1/catalog/us'; // 'us' can be replaced with desired storefront
 
-// Search songs on Apple Music
+//recommendations requires user subscription to apple music
+//get categories/generes
+  //requires genre id,returns name of genre?
+//ratings requires user subscription to apple music
+//
+
+// search for category resources
+  //shows search for songs, can also search for:
+    //activities, albums, apple-curators, artists, curators, music-videos, playlists, record-labels, stations
 export async function searchAppleMusicSongs(query: string, limit: number = 10) {
   try {
     const encodedQuery = encodeURIComponent(query);
-    const response = await fetch(`${APPLE_MUSIC_API_URL}/search?term=${encodedQuery}&types=songs&limit=${limit}`, {
+    const response = await fetch(`${APPLE_MUSIC_API_URL}/search?term=${encodedQuery}&types=songs&limit=${limit}`, { //replace types=songs with albums or artists for differing results
       method: 'GET',
       headers: {
         Authorization: `Bearer ${APPLE_MUSIC_DEVELOPER_TOKEN}`,
@@ -27,14 +35,9 @@ export async function searchAppleMusicSongs(query: string, limit: number = 10) {
 }
 
 
-export async function getAppleRecommendations(query:string, limit:number=10) {
-    const encodedId = encodeURIComponent("1443147095");
-    const response = await fetch(`https://api.music.apple.com/v1/me/recommendations/${encodedId}`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${APPLE_MUSIC_DEVELOPER_TOKEN}`,
-        },
-    });
-    const data = await response.json();
-    return data;
-}
+//get charts
+  //albums, music-videos, playlists, songs
+
+
+
+//
